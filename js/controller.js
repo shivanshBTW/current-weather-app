da0a6fc132f4bcb5967e8823fb52d4e77917d9b4
Shivansh Tyagi <stshivansh99@gmail.com>
2018-06-24 17:23:19 +0530

    1
    first working version with some possible bugs

---
+++ js/controller.js
@@ -0,0 +1,76 @@
+window.addEventListener('load', init);
+
+function init() {
+    bindEvents();
+    fillDefault();
+}
+
+function bindEvents() {
+    document.querySelector('#searchButton').addEventListener('click', callAPI);
+}
+
+function fillDefault() {
+    document.querySelector('#cityName').value = 'Delhi';
+
+}
+
+function callAPI() {
+    var city = document.querySelector('#cityName').value;
+    var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=097cae123862b91724e8f4f5e69d7050";
+    console.log(url);
+    fetch(url).then(received => {
+        console.log('recieved is ', received);
+        received.json().then(data => {
+            console.log('data is : ', data);
+            printWeather(data);
+        }).catch(err => {
+            console.log('error in data is ', err);
+        })
+    }).catch(err => {
+        console.log('err in received is ', err);
+    })
+
+
+}
+
+function printImage(data) {
+    console.log('here');
+
+}
+
+function printWeather(data) {
+    document.querySelector('#minTemp').innerText = data.main.temp_min - 273.15;
+    document.querySelector('#maxTemp').innerText = data.main.temp_max - 273.15;
+    document.querySelector('#avgTemp').innerText = (parseInt(document.querySelector('#minTemp').innerText) + parseInt(document.querySelector('#maxTemp').innerText)) / 2;
+    document.querySelector('#clouds').innerText = data.clouds.all + "%";
+    document.querySelector('#humidity').innerText = data.main.humidity + "%";
+    document.querySelector('#weather').innerText = data.weather[0].main;
+    document.querySelector('#windSpeed').innerText = data.wind.speed + " m/s";
+    // document.querySelector('#gustSpeed').innerText = data.wind.gust + " m/s";
+    var dirDegree = data.wind.deg / 22.5;
+    var direction;
+    console.log(dirDegree);
+    if (dirDegree >= 2 && dirDegree < 4) {
+        direction = "North-East";
+    } else if (dirDegree >= 4 && dirDegree < 6) {
+        direction = "East";
+    } else if (dirDegree >= 6 && dirDegree < 8) {
+        direction = "South-East";
+    } else if (dirDegree >= 8 && dirDegree < 10) {
+        direction = "South";
+    } else if (dirDegree >= 10 && dirDegree < 12) {
+        direction = "South-West";
+    } else if (dirDegree >= 12 && dirDegree < 14) {
+        direction = "West";
+    } else if (dirDegree >= 4 && dirDegree < 6) {
+        direction = "North-West";
+    } else if (dirDegree >= 17 || dirDegree < 2) {
+        direction = "North";
+    }
+    document.querySelector('#direction').innerText = direction
+
+
+
+
+
+}
